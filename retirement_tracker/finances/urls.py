ff588060a05859e44836dfc715da951e7066fb5e
Rene Valdez <elhombrefiero@outlook.com>
2022-12-10 18:27:20 -0800

    Added some views for Users and Accounts.

---
+++ retirement_tracker/finances/urls.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Python Library Imports
+
+# Other Imports
+from django.urls import path
+from django.views.generic import TemplateView
+
+from finances import views
+
+app_name = 'finances'
+
+urlpatterns = [
+    # Ex. /finances/
+    path('', views.index, name='index'),
+    # Ex. /finances/user_overview=id
+    path('user_overview=<int:user_id>/', views.user_main, name='user_overview'),
+    # Ex. /finances/account_overview=id
+    path('account_overview=<int:account_id>/', views.account_overview, name='account_overview')
+]
--- retirement_tracker/finances/views.py
+++ retirement_tracker/finances/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from finances.models import User, Account
+
 # Create your views here.
+
+
+def index(request):
+    """ Index of finance tracker. Contains a list of users. """
+    users = User.objects.all()
+
+    return render(request, 'finances/index.html', {'users': users})
+
+
+def user_main(request, user_id: int):
+    """ Contains links for each account for the user. """
+    user = User.objects.get(id=user_id)
+    user_accounts = Account.objects.filter(user=user)
+
+    return render(request, 'finances/user_overview.html', {'user': user,
+                                                           'accounts': user_accounts})
+
+
+def account_overview(request, account_id: int):
+    account = Account.objects.get(id=account_id)
+    balance = account.return_balance()
+
+    return render(request, 'finances/account_overview.html', {'account': account, 'balance': balance})
