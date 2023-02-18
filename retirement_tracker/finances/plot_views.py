33c23a87f0521ddc8611da999e99a019ac0452ec
Rene <renemv@gmail.com>
2023-02-17 21:27:48 -0800

    Created budgeted and actual expense pie charts

--- retirement_tracker/finances/plot_views.py
+++ retirement_tracker/finances/plot_views.py
@@ -1,15 +1,97 @@
 #!/usr/bin/env python3
 
 from django.http import JsonResponse
+from django.shortcuts import redirect
 from django.views.generic import DetailView
 
-from finances.models import User
+from finances.models import User, MonthlyBudget
+from finances.utils import chartjs_utils as cjs
+
+
+def get_pie_chart_config(name):
+    "Returns the configuration for a pie chart minus the data using chart.js"
+    config = {}
+    config.update({
+        'type': 'pie',
+        'options': {
+            'responsive': True,
+            'plugins': {
+                'legend': {
+                    'position': 'top',
+                },
+                'title': {
+                    'display': True,
+                    'text': f'{name}'
+                }
+            }
+        }
+    }
+    )
+    return config
 
 
 class MonthlyBudgetPlotView(DetailView):
     model = User
 
+    def dispatch(self, request, *args, **kwargs):
+        self.month = kwargs['month']
+        self.year = kwargs['year']
+        userpk = kwargs['pk']
+        self.user = User.objects.get(pk=userpk)
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, *args, **kwargs):
-        print(f"args: {args}")
-        print(f"kwargs: {kwargs}")
-        return JsonResponse({'test': 1})
+        try:
+            mb = MonthlyBudget.objects.get(month=self.month, year=self.year, user=self.user)
+        except MonthlyBudget.DoesNotExist:
+            return redirect('finances:user_add_monthly_budget_month_year', self.user.pk, self.month, self.year)
+        config = get_pie_chart_config('Budgeted')
+        data = {
+            'labels': ['Mandatory', 'Statutory', 'Mortgage', 'Debts, Goals, Retirement', 'Discretionary'],
+            'datasets': [
+                {
+                    'label': 'Budgeted',
+                    'data': [mb.mandatory, mb.statutory, mb.mortgage, mb.debts_goals_retirement, mb.discretionary],
+                    'backgroundColor': [cjs.get_color('red'), cjs.get_color('orange'), cjs.get_color('yellow'), cjs.get_color('green'), cjs.get_color('blue')],
+                }
+            ]
+        }
+        return_dict = {}
+        return_dict['config'] = config
+        return_dict['data'] = data
+
+        return JsonResponse(return_dict)
+
+
+class ActualExpensesByBudgetGroup(DetailView):
+    model = User
+
+    def dispatch(self, request, *args, **kwargs):
+        self.month = kwargs['month']
+        self.year = kwargs['year']
+        userpk = kwargs['pk']
+        self.user = User.objects.get(pk=userpk)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+
+        mand_act, mort_act, dgr_act, disc_act, stat_act = self.user.return_tot_expenses_by_budget_month_year(self.month,
+                                                                                                             self.year)
+        config = get_pie_chart_config('Actual')
+
+        data = {
+            'labels': ['Mandatory', 'Statutory', 'Mortgage', 'Debts, Goals, Retirement', 'Discretionary'],
+            'datasets': [
+                {
+                    'label': 'Actual',
+                    'data': [mand_act, stat_act, mort_act, dgr_act, disc_act],
+                    'backgroundColor': [cjs.get_color('red'), cjs.get_color('orange'), cjs.get_color('yellow'),
+                                        cjs.get_color('green'), cjs.get_color('blue')],
+                }
+            ]
+        }
+        return_dict = {}
+        return_dict['config'] = config
+        return_dict['data'] = data
+
+        return JsonResponse(return_dict)
--- retirement_tracker/finances/urls/plot_urls.py
+++ retirement_tracker/finances/urls/plot_urls.py
@@ -10,6 +10,10 @@ from finances import plot_views as pv
 app_name = 'finances'
 
 urlpatterns = [
-    # Ex. /finances/plot/user/1/monthly_budget/January/2022/actual
-    path('plot/user/<int:pk>/monthly_budget/<str:month>/<int:year>/actual', pv.MonthlyBudgetPlotView.as_view(), name='plot_monthlybudget_for_user')
+    # Ex. /finances/plot/user/1/monthly_budget/January/2022/budgeted
+    path('plot/user/<int:pk>/monthly_budget/<str:month>/<int:year>/budgeted', pv.MonthlyBudgetPlotView.as_view(),
+         name='plot_monthlybudget_for_user'),
+    # Ex. /finances/plot/user/1/expenses/January/2022/by_budgetgroup
+    path('plot/user/<int:pk>/expenses/<str:month>/<int:year>/by_budgetgroup', pv.ActualExpensesByBudgetGroup.as_view(),
+         name='plot_expenses_by_budget_group'),
 ]
