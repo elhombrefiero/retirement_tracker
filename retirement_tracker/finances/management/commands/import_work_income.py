3f855b8a5d5a701bfa375fe3faeb7947ab94befc
Rene - Desktop <renemv@gmail.com>
2025-03-18 19:51:12 -0700

    Added more detail on the import work income script.

--- retirement_tracker/finances/management/commands/import_work_income.py
+++ retirement_tracker/finances/management/commands/import_work_income.py
@@ -10,7 +10,7 @@ import shlex
 import json
 import csv
 
-from finances.models import User, RetirementAccount, Deposit, Withdrawal, BUDGET_GROUP_DGR
+from finances.models import User, RetirementAccount, Deposit, Withdrawal, BUDGET_GROUP_DGR, CheckingAccount
 from finances.utils.file_processing import process_user_work_file
 
 # Other Imports
@@ -61,20 +61,21 @@ class Command(BaseCommand):
         """
         parser.add_argument('filename', type=Path, help='Work income file.')
         parser.add_argument('user', type=str, help='User name (case-sensitive)')
-        parser.add_argument('Checking account name', type=str, help='Checking account name (case-sensitive)')
-        parser.add_argument('Savings account name', type=str, help='Savings account name (case-sensitive). Enter "None" if no savings account.')
-        parser.add_argument('401k account name', type=str, help='401k account name (case-sensitive)')
-        parser.add_argument('HSA account name', type=str, help='HSA account name (case-sensitive)')
+        parser.add_argument('check_account', type=str, help='Checking account name (case-sensitive)')
+        parser.add_argument('saving_account', type=str, help='Savings account name (case-sensitive). Enter "None" if '
+                                                             'no savings account.')
+        parser.add_argument('ret401k_account', type=str, help='401k account name (case-sensitive)')
+        parser.add_argument('retHSA_account', type=str, help='HSA account name (case-sensitive)')
 
     def handle(self, *args, **kwargs):
         """ Processes the work-income file and information based on user input."""
-        user_input = kwargs['user']
-        account_name = kwargs['account name']
         filename = kwargs['filename']
-        difference = 0.0
-        number_of_entries = 0
-        min_date = None
-        max_date = None
+        user_input = kwargs['user']
+        caccount_name = kwargs['check_account']
+        saccount_name = kwargs['saving_account']
+        ret401k_account_name = kwargs['ret401k_account']
+        retHSA_account_name = kwargs['retHSA_account']
+
         try:
             user = User.objects.get(name=user_input)
         except User.DoesNotExist:
@@ -85,75 +86,48 @@ class Command(BaseCommand):
             return
 
         try:
-            account = RetirementAccount.objects.get(user=user, name=account_name)
-        except RetirementAccount.DoesNotExist:
-            print(f'Account {account_name} does not exist for user {user_input}. Here are valid options: ')
-            ret_accounts = RetirementAccount.objects.filter(user=user)
-            for ret_account in ret_accounts:
-                print(ret_account.name)
+            caccount = CheckingAccount.objects.get(user=user, name=caccount_name)
+        except CheckingAccount.DoesNotExist:
+            print(f'Account {caccount_name} does not exist for user {user_input}. Here are valid options: ')
+            c_accounts = CheckingAccount.objects.filter(user=user)
+            for c_account in c_accounts:
+                print(c_account.name)
             return
 
-        with open(filename) as csvobj:
-            dr = csv.DictReader(csvobj)
-            if kwargs['lookup_date'] not in dr.fieldnames:
-                print(f"ERROR: lookup for date ({kwargs['lookup_date']}) does not exist in data. Cannot continue.")
-                return
-            if kwargs['lookup_transaction'] not in dr.fieldnames:
-                print(
-                    f"ERROR: lookup for transactions ({kwargs['lookup_transaction']}) does not exist in data. Cannot continue.")
-                return
-            if kwargs['lookup_amount'] not in dr.fieldnames:
-                print(
-                    f"ERROR: lookup for amount ({kwargs['lookup_amount']}) does not exist in data. Cannot continue.")
+        saccount = None
+        if saccount_name:
+            try:
+                saccount = CheckingAccount.objects.get(user=user, name=saccount_name)
+            except CheckingAccount.DoesNotExist:
+                print(f'Account {saccount_name} does not exist for user {user_input}. Here are valid options: ')
+                c_accounts = CheckingAccount.objects.filter(user=user)
+                for c_account in c_accounts:
+                    print(c_account.name)
                 return
 
-            for row in dr:
-                date = row[kwargs['lookup_date']]
-                date_dt = datetime.strptime(date, kwargs['lookup_date_format'])
-                if min_date is None:
-                    min_date = date_dt
-                else:
-                    min_date = min(min_date, date_dt)
-                if max_date is None:
-                    max_date = date_dt
-                else:
-                    max_date = max(max_date, date_dt)
-                amount = float(row[kwargs['lookup_amount']])
-                transaction = row[kwargs['lookup_transaction']]
-                if transaction == kwargs['lookup_withdrawal']:
-                    withdrawal, created = Withdrawal.objects.update_or_create(
-                        account=account,
-                        date=date_dt,
-                        description=transaction,
-                        amount=amount,
-                        defaults={'budget_group': BUDGET_GROUP_DGR,
-                                  'category': 'Retirement',
-                                  'location': 'Work'}
-                    )
-                    if created:
-                        print(f'Created withdrawal {withdrawal}')
-                    withdrawal.save()
-                    difference -= amount
-                    number_of_entries += 1
+        try:
+            ret401kaccount = RetirementAccount.objects.get(user=user, name=ret401k_account_name)
+        except RetirementAccount.DoesNotExist:
+            print(f'Account {ret401k_account_name} does not exist for user {user_input}. Here are valid options: ')
+            c_accounts = RetirementAccount.objects.filter(user=user)
+            for c_account in c_accounts:
+                print(c_account.name)
+            return
 
-                if transaction == kwargs['lookup_deposit']:
-                    if kwargs['deposit_description']:
-                        dep_description = kwargs['deposit_description']
-                    else:
-                        dep_description = transaction
-                    deposit, created = Deposit.objects.update_or_create(account=account,
-                                                                        date=date_dt,
-                                                                        amount=amount,
-                                                                        description=dep_description,
-                                                                        defaults={'category': 'Retirement',
-                                                                                  'location': 'Work'}
-                                                                        )
-                    if created:
-                        print(f'Created deposit: {deposit}')
-                    deposit.save()
-                    difference += amount
-                    number_of_entries += 1
+        try:
+            rethsaaccount = RetirementAccount.objects.get(user=user, name=retHSA_account_name)
+        except RetirementAccount.DoesNotExist:
+            print(f'Account {retHSA_account_name} does not exist for user {user_input}. Here are valid options: ')
+            c_accounts = RetirementAccount.objects.filter(user=user)
+            for c_account in c_accounts:
+                print(c_account.name)
+            return
+
+        if not os.path.exists(filename):
+            print(f'File {filename} does not exist.')
+            return
+
+        work_info = process_user_work_file(filename)
+
         self.stdout.write(self.style.SUCCESS(
-            f'Min date: {min_date}. Max date: {max_date}'))
-        self.stdout.write(self.style.SUCCESS(
-            f'Processed {number_of_entries} entries for {account.name}. Difference between deposits and withdrawals: {difference}'))
+            f'Processed {filename} for work income.'))
--- retirement_tracker/finances/utils/file_processing.py
+++ retirement_tracker/finances/utils/file_processing.py
@@ -1,5 +1,10 @@
 
+from datetime import datetime
 from pypdf import PdfReader
+import logging
+import re
+
+logger = logging.getLogger(__name__)
 
 
 def process_user_work_file(user_work_file):
@@ -10,13 +15,46 @@ def process_user_work_file(user_work_file):
             taxes.
         Returns a dictionary with the information, which can then be fed into another view. """
 
+    pay_date_regex = re.compile(r'Pay Date ([0-9]{2}/[0-9]{2}/[0-9]{4})')
+
+    return_dict = dict()
+
     pdf_obj = PdfReader(user_work_file)
     page = pdf_obj.pages[0]
     split_page = page.extract_text().split('\n')
+
+    for i, line in enumerate(split_page):
+        pay_date_match = pay_date_regex.match(line)
+        if pay_date_match:
+            pay_date = pay_date_match.group(1)
+            pay_datetime = datetime.strptime(pay_date, '%m/%d/%Y')  # TODO: Apply the time zone
+            print(f'Found Pay date: {pay_datetime}')
+            continue
+        if 'Earnings' in line:
+            # Pay type, hours, pay rate, current, ytd
+
+            # Go through the following earnings lines and pull the Current values
+            pass
+
+        if 'Deductions' in line:
+            # Deduction, Pre-Tax, Employee Current, Employee YTD, Employer Current, Employer YTD
+            pass
+
+        if 'Taxes' in line:
+            # Tax, Current, YTD
+            pass
+
+        if 'Net Pay Distribution' in line:
+            # Grab the data used to transfer to the Savings account
+            pass
+
+
+        print(line)
 
     # Earnings
 
     # Deductions
 
     # Taxes
-    pass
+
+    return return_dict
