f8c043f6d6497608876505cc36f8ac49919f1479
Rene - Desktop <renemv@gmail.com>
2025-04-11 13:58:50 -0700

    Created a management function that allows the user to upload one or more work income pdf files to add the income and process taxes.

--- retirement_tracker/finances/management/commands/import_work_income.py
+++ retirement_tracker/finances/management/commands/import_work_income.py
@@ -2,19 +2,15 @@
 
 # Python Library Imports
 from pathlib import Path
+import glob
 import os
-import shutil
-from datetime import datetime
-import subprocess as sp
-import shlex
-import json
-import csv
 
-from finances.models import User, RetirementAccount, Deposit, Withdrawal, BUDGET_GROUP_DGR, CheckingAccount
+from finances.models import User, RetirementAccount, Deposit, CheckingAccount, BUDGET_GROUP_DGR, Transfer, Statutory
 from finances.utils.file_processing import process_user_work_file
 
 # Other Imports
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 
 
 # Defined Functions:
@@ -58,8 +54,11 @@ class Command(BaseCommand):
 
         Net pay Distribution is going to have account numbers, account type, and the amount put into that account based on after tax and deductions.
 
+        Deductions are entered as Debts, Goals, Retirement because they are all 401k or HSA related
+
         """
-        parser.add_argument('filename', type=Path, help='Work income file.')
+        parser.add_argument('filename', type=Path,
+                            help='Work income file.')
         parser.add_argument('user', type=str, help='User name (case-sensitive)')
         parser.add_argument('check_account', type=str, help='Checking account name (case-sensitive)')
         parser.add_argument('saving_account', type=str, help='Savings account name (case-sensitive). Enter "None" if '
@@ -69,7 +68,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         """ Processes the work-income file and information based on user input."""
-        filename = kwargs['filename']
+        input_file = kwargs['filename']
         user_input = kwargs['user']
         caccount_name = kwargs['check_account']
         saccount_name = kwargs['saving_account']
@@ -85,6 +84,7 @@ class Command(BaseCommand):
                 print(user.name)
             return
 
+        caccount = None
         try:
             caccount = CheckingAccount.objects.get(user=user, name=caccount_name)
         except CheckingAccount.DoesNotExist:
@@ -123,11 +123,97 @@ class Command(BaseCommand):
                 print(c_account.name)
             return
 
-        if not os.path.exists(filename):
-            print(f'File {filename} does not exist.')
+        files = list()
+        if not os.path.exists(input_file):
+            print(f'File {input_file} does not exist.')
             return
 
-        work_info = process_user_work_file(filename)
+        if os.path.isdir(input_file):
+            files = glob.glob(os.path.join(input_file, '*.pdf'))
+        else:
+            files.append(input_file)
+
+        self.stdout.write(self.style.SUCCESS(f'Found {len(files)} pdf files to process.'))
+
+        for filename in files:
+            self.stdout.write(self.style.SUCCESS(f'Processing {filename}.'))
+            work_info = process_user_work_file(filename)
+
+            if work_info:
+                try:
+                    pdate = work_info['pay_date']
+                except:
+                    self.stdout.write(self.style.ERROR(f'Could not extract pay date from {filename}.'))
+                    return
+                self.stdout.write(self.style.SUCCESS(f'Pay date is {pdate}.'))
+
+                if 'earnings' in work_info.keys():
+                    for description in work_info['earnings'].keys():
+                        try:
+                            deposit, created = Deposit.objects.update_or_create(account=caccount,
+                                                                                date=pdate,
+                                                                                description=description,
+                                                                                amount=work_info['earnings'][description],
+                                                                                defaults={'category': 'Work',
+                                                                                          'location': 'Work'})
+                            if created:
+                                self.stdout.write(self.style.SUCCESS(f'Created Deposit {deposit}'))
+                            else:
+                                self.stdout.write(self.style.SUCCESS(f'Updated Deposit {deposit}'))
+                            deposit.save()
+                        except IntegrityError as e:
+                            self.stdout.write(self.style.ERROR(f'Error updating Deposit {description} on {pdate}: {e}'))
+                            continue
+                        deposit.save()
+
+                # TODO: Determine if deductions should come through income file or through associated account files.
+                # if 'deductions' in work_info.keys():
+                #     for description in work_info['deductions'].keys():
+                #         if 'employee' in work_info['deductions'][description].keys():
+                #             withdrawal, created = Withdrawal.objects.update_or_create(account=caccount,
+                #                                       date=pdate,
+                #                                       description=description,
+                #                                       amount=work_info['deductions'][description]['employee'],
+                #                                       defaults={'budget_group': 'BUDGET_GROUP',
+                #                                                 'category': 'Work',
+                #                                                 'location': 'Work'})
+                if 'taxes' in work_info.keys():
+                    for description in work_info['taxes'].keys():
+                        try:
+                            statutory, created = Statutory.objects.update_or_create(amount=work_info['taxes'][description],
+                                                                                    date=pdate,
+                                                                                    description=description,
+                                                                                    defaults={'user': user,
+                                                                                              'category': 'Work',
+                                                                                              'location': 'Work'})
+                            if created:
+                                self.stdout.write(self.style.SUCCESS(f'Created Statutory {statutory}'))
+                            else:
+                                self.stdout.write(self.style.SUCCESS(f'Updated Statutory {statutory}'))
+                        except IntegrityError as e:
+                            self.stdout.write(self.style.ERROR(f'Error updating Statutory {description} on {pdate}: {e}'))
+                            continue
+                        statutory.save()
+                if 'transfer' in work_info.keys():
+                    try:
+                        transfer, created = Transfer.objects.update_or_create(account_from=caccount,
+                                                                              account_to=saccount,
+                                                                              date=pdate,
+                                                                              budget_group=BUDGET_GROUP_DGR,
+                                                                              amount=work_info['transfer'],
+                                                                              defaults={'category': 'Transfer',
+                                                                                        'description': 'Work Income',
+                                                                                        'location': 'Work'})
+
+                        if created:
+                            self.stdout.write(self.style.SUCCESS(
+                                f'Transfered ${work_info["transfer"]} from {caccount_name} to {saccount_name}'))
+                            transfer.save()
+                    except IntegrityError as e:
+                        self.stdout.write(self.style.ERROR(
+                            f'Error updating Transfer of {work_info["transfer"]} from {caccount_name} to {saccount_name}: {e}'))
+            else:
+                self.stdout.write(self.style.ERROR(f'No information found for {filename}'))
 
         self.stdout.write(self.style.SUCCESS(
-            f'Processed {filename} for work income.'))
+            f'Completed processing import_worK_incomes.'))
